7262a48f63b2f735ed52186bfafb6f7de1382686
WindzR <bodins@yandex.ru>
2020-12-09 02:09:09 +0300

    2.1. Тест на ValidateInput [#389992]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(2));
+    }
+
+    @Test
+    public void whenMultiValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2", "1", "0"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected1 = input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        int selected3 = input.askInt("Enter menu:");
+        int[] selected = {selected1, selected2, selected3};
+        int[] expected = {2, 1, 0};
+        assertArrayEquals(selected, expected);
+    }
+
+    @Test
+    public void whenNegativeNumInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-2));
+    }
+}
