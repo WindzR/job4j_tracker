fcca235e4a92ccdeebf0384d60ca4f47700da8c6
WindzoR <windzr67@gmail.com>
2020-12-12 18:22:37 +0300

    2. Singleton. [#390002]

---
+++ src/main/java/ru/job4j/tracker/TrackerSingle2.java
@@ -0,0 +1,93 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class TrackerSingle2 {
+    private static TrackerSingle2 instance;
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    private TrackerSingle2() {
+    }
+
+    public static TrackerSingle2 getInstance() {
+        if (instance == null) {
+            instance = new TrackerSingle2();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findByAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[this.size];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(name)) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean exeption = index != -1;
+        if (exeption) {
+            items[index] = item;
+            item.setId(id);
+        }
+        return exeption;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean exeption = index != -1;
+        if (exeption) {
+            System.arraycopy(items, index + 1, items, index, this.size - index);
+            items[this.size - 1] = null;
+            this.size--;
+        }
+        return exeption;
+    }
+
+    @Override
+    public String toString() {
+        return "Tracker{" +
+                "items=" + Arrays.toString(items) +
+                ", ids=" + ids +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        TrackerSingle2 tracker = TrackerSingle2.getInstance();
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/TrackerSingle3.java
@@ -0,0 +1,90 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class TrackerSingle3 {
+    private static final TrackerSingle3 INSTANCE = new TrackerSingle3();
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    private TrackerSingle3() {
+    }
+
+    public static TrackerSingle3 getInstance() {
+        return INSTANCE;
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findByAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[this.size];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(name)) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean exeption = index != -1;
+        if (exeption) {
+            items[index] = item;
+            item.setId(id);
+        }
+        return exeption;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean exeption = index != -1;
+        if (exeption) {
+            System.arraycopy(items, index + 1, items, index, this.size - index);
+            items[this.size - 1] = null;
+            this.size--;
+        }
+        return exeption;
+    }
+
+    @Override
+    public String toString() {
+        return "Tracker{" +
+                "items=" + Arrays.toString(items) +
+                ", ids=" + ids +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        TrackerSingle3 tracker = TrackerSingle3.getInstance();
+    }
+}
