3c83573e64df7928b24403d8eefac469b4812a02
WindzoR <windzr67@gmail.com>
2021-05-16 20:33:08 +0300

    1. Sql схема для проекта Tracker. [#475429]

---
+++ db/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO items (name) VALUES ('Petr');
+INSERT INTO items (name) VALUES ('Dmitry');
+INSERT INTO items (name) VALUES ('Stepan');
+INSERT INTO items (name) VALUES ('Olga');
+INSERT INTO items (name) VALUES ('Marussia');
+INSERT INTO items (name) VALUES ('Svetlana');
+INSERT INTO items (name) VALUES ('Oleg');
+INSERT INTO items (name) VALUES ('Vladimir');
+INSERT INTO items (name) VALUES ('Aleksandra');
+INSERT INTO items (name) VALUES ('Ivan');
